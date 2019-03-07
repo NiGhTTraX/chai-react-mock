e440aeda63823e36049f6f08070ecb47ce23ee8e
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-03-07 22:28:04 +0100

    Overload Assertion to only return `rendered` for ReactMock

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,39 @@
+import { ReactMock } from 'react-mock-component';
+
+declare global {
+  export namespace Chai {
+    export interface ReactMockAssertion<Props> extends ReactMockLanguageChains<Props> {
+      rendered: boolean;
+    }
+
+    export interface ReactMockLanguageChains<Props> {
+      to: ReactMockAssertion<Props>;
+      have: ReactMockAssertion<Props>;
+      been: ReactMockAssertion<Props>;
+      not: ReactMockAssertion<Props>;
+    }
+
+    interface AssertionStatic {
+      <Props>(target: ReactMock<Props>, message?: string): ReactMockAssertion<Props>;
+    }
+  }
+}
+
+interface ChaiAssertion {
+  addProperty: (name: string, fn: () => void) => void;
+}
+
 // eslint-disable-next-line no-redeclare
-export default ({ Assertion }: { Assertion: any }) => {
+export default ({ Assertion }: { Assertion: ChaiAssertion }) => {
   Assertion.addProperty('rendered', function rendered() {
     // @ts-ignore
     const Component = this._obj;
 
     // @ts-ignore
-    this.assert(Component.rendered === true, 'Expected component to have been rendered');
+    this.assert(
+      Component.rendered,
+      'Expected component to have been rendered',
+      'Expected component to not have been rendered'
+    );
   });
 };
--- tests/node/index.spec.tsx
+++ tests/node/index.spec.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import { describe, it, expect, $render } from './suite';
 import { createReactStub } from 'react-mock-component';
+import { describe, it, expect, $render } from './suite';
 
 describe('ChaiReactMock', () => {
   it('should assert whether a component was rendered', () => {
     const Mock = createReactStub();
 
     expect(Mock).to.not.have.been.rendered;
-    expect(() => expect(Mock).to.have.been.rendered).to.throw();
+    expect(() => expect(Mock).to.have.been.rendered).to.throw(
+      'Expected component to have been rendered'
+    );
 
     $render(<Mock />);
 
     expect(Mock).to.have.been.rendered;
-    expect(() => expect(Mock).to.not.have.been.rendered).to.throw();
+    expect(() => expect(Mock).to.not.have.been.rendered).to.throw(
+      'Expected component to not have been rendered'
+    );
   });
 });
